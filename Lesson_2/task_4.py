5e40a757f12f7ac12582b330e867783c49b590c6
nutika03 <nutika03@gmail.com>
2025-11-10 22:12:47 +0200

    changed task_4

--- Lesson_2/task_4.py
+++ Lesson_2/task_4.py
@@ -1,4 +1,5 @@
 price = int(input('Enter price: '))
 discount = int(input('Enter discount %: '))
-price_final = float(price - (price * discount // 100))
+price_final = float(price - (price * discount / 100))
 print('price with discount: ', price_final)
+
