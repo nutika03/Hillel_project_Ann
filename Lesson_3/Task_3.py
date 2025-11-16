40d8dde073ee3b8b689a2e4a53238a748ac07287
nutika03 <nutika03@gmail.com>
2025-11-16 20:29:41 +0200

    Add HW-Lesson 3 task_3

--- Lesson_3/Task_3.py
+++ Lesson_3/Task_3.py
@@ -3,6 +3,17 @@ if len(general_list) == 0:
     result = [[], []]
     print(result)
 else:
+    even = len(general_list)
+    if even % 2 == 0:
+        two_lists = len(general_list) // 2
+    else:
+        two_lists = len(general_list) // 2 + 1
+    first_list = general_list[:two_lists]
+    second_list = general_list[two_lists:]
+    result = [first_list, second_list]
+    print(result)
+
+
 
 
 
